29d721359c79408ad591e3ce0b444ff8f99bdc9a
TJRobson <thomas.j.robson@hotmail.com>
2017-09-07 14:17:49 +1200

    End of part 6, context variables.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,18 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    html_var = 'f string'
-    html_ = f"""<DOCTYPE html>
-    <html lang=en>
 
-    <head>
-    </head>
-    <body>
-        <h1>Hello World</h1>
-        <p>This is {html_var} comming through</p>
-    </body>
-    </html>
-    """
-    #python 3.6 f strings
-    return HttpResponse(html_)
-    #return render(request, 'home.html', {})
+    return render(request, 'base.html', {'html_var': 'context variable'})
