d8b34dcc6a01a57f3fd1d20ad22ff002e711e281
TJRobson <thomas.j.robson@hotmail.com>
2017-09-07 13:57:06 +1200

    python 3.6 f strings.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('hello')
+    html_var = 'f string'
+    html_ = f"""<DOCTYPE html>
+    <html lang=en>
+
+    <head>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <p>This is {html_var} comming through</p>
+    </body>
+    </html>
+    """
+    #python 3.6 f strings
+    return HttpResponse(html_)
     #return render(request, 'home.html', {})
