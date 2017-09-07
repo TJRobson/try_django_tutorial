c1d55fd2f1658d5747d914b1857fd09b42814c67
TJRobson <thomas.j.robson@hotmail.com>
2017-09-07 21:53:49 +1200

    End of part 16, all examples work from this point. Fixed templates. Getting data from database.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
 
-# Create your views here.
-class HomeView(TemplateView):
-    template_name = 'home.html'
+from .models import RestaurantLocation
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
+# Create your views here.
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
