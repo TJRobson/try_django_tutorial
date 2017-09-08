c700cd1d55bcb4ef9ee57c27b917cbfd749e00b2
TJRobson <thomas.j.robson@hotmail.com>
2017-09-08 16:13:00 +1200

    End of part 19. DetailView s.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,8 +1,8 @@
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import RestaurantLocation
 
@@ -28,3 +28,17 @@ class RestaurantListView(ListView):
         else:
             queryset = RestaurantLocation.objects.all()
         return queryset
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        print(self.kwargs)
+        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id) #pk = rest_id
+        return obj
