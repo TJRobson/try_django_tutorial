190271b22d938cc1d7a6b2d8dbc67f2e04901961
TJRobson <thomas.j.robson@hotmail.com>
2017-09-22 15:44:57 +1200

    End of part 38. Set up Profiles app - view.

---
+++ src/profiles/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+
+User = get_user_model()
+
+# Create your views here.
+
+class ProfileDetailView(DetailView):
+    # queryset = User.objects.filter(is_active=True)
+    template_name = 'profiles/user.html'
+
+    def get_object(self):
+        username = self.kwargs.get('username')
+        if username == None:
+            raise Http404
+        return get_object_or_404(User, username__iexact=username, is_active=True)
