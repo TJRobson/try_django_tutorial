bfc1dc3ab2b0a82431cbbe44524aa361ebb83a76
TJRobson <thomas.j.robson@hotmail.com>
2017-10-16 23:57:47 +1300

    End of part 45. Activation Keys.

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
-
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from .utils import code_generator
 User = settings.AUTH_USER_MODEL
 # Create your models here.
 
@@ -18,12 +20,13 @@ class ProfileManager(models.Manager):
           return profile_, is_following
 
 class Profile(models.Model):
-    user        = models.OneToOneField(User)
-    followers   = models.ManyToManyField(User, related_name='is_following', blank=True)
-#    following   = models.ManyToManyField(User, related_name='following', blank=True)
-    timestamp   = models.DateTimeField(auto_now_add=True)
-    updated     = models.DateTimeField(auto_now=True)
-    activated   = models.BooleanField(default=False)
+    user           = models.OneToOneField(User)
+    followers      = models.ManyToManyField(User, related_name='is_following', blank=True)
+#    following      = models.ManyToManyField(User, related_name='following', blank=True)
+    timestamp      = models.DateTimeField(auto_now_add=True)
+    updated        = models.DateTimeField(auto_now=True)
+    activated      = models.BooleanField(default=False)
+    activation_key = models.CharField(max_length=120, blank=True, null=True)
 
     objects = ProfileManager()
 
@@ -31,8 +34,27 @@ class Profile(models.Model):
         return self.user.username
 
     def send_activation_email(self):
-        print("Activation")
-        pass
+        #print("Activation")
+        if not self.activated:
+            self.activation_key = code_generator()#'somekey' gen key
+            self.save()
+            path_ = reverse('activate', kwargs={"code": self.activation_key})
+            subject = 'Activate Account'
+            from_email = settings.DEFAULT_FROM_EMAIL
+            message = f'Activate your account here: {path_}'
+            recipient_list = [self.user.email]
+            html_message = f'<p>Activate your account here: {path_}</p>'
+            print(html_message)
+            # sent_mail = send_mail(
+            #                 subject,
+            #                 message,
+            #                 from_email, 
+            #                 recipient_list,
+            #                 fail_silently=False,
+            #                 html_message=html_message)
+            sent_mail = False
+            return sent_mail
+
 
 def post_save_user_receiver(sender, instance, created, *args, **kwargs):
     if created:
